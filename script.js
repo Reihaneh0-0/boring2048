db809fbb51ae003e311620397fa3b79233f55af9
Reihaneh0-0 <reihan.38dt@gmail.com>
2025-07-17 09:06:20 +0330

    +Add cells

--- script.js
+++ script.js
@@ -16,3 +16,24 @@ const size = 4
 
      let board = Array.from({ length: size }, () => Array(size).fill(0));
      let prevBoard = board.map( r=>[...r] );
+
+     for (let i=0 ; i < size * size ; i++) {
+        const cell=document.creatElement('div')
+        cell.className = 'cell w-[20vw] h-[20vw] rounded-lg sm:rounded-xl md:h-32 md:w-32 md:rounded-2xl';
+         grid.appendChild(cell);
+     }
+
+     function spawn() { 
+     }
+
+     function slide(row) {
+        row = row.filter(Boolean);
+        for (let i = 0 ; i < row.length - 1 ; i++)
+            if ( row[i] === row[i + 1]){
+                row[i] *=2;
+                row.splice(i+1 , 1);
+            }
+     }
+
+     return row;
+    }
